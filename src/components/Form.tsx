c55b328bfbbf05be46dad0e4eaca545e38e5a1a8
Thercístenes <thercistenes1@outlook.com>
2023-07-13 11:16:43 -0300

    push para duvidas no slack

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,33 +1,63 @@
 import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { object, string } from 'yup';
+
+const schema = object({
+  nome: string().min(1),
+  login: string().min(1),
+  senha: string()
+    .min(8, 'Possuir 8 ou mais caracteres')
+    .max(16, 'Possuir até 16 caracteres')
+    .matches(/^(?=.*[a-zA-Z])(?=.*[0-9])/, 'Possuir letras e números')
+    .matches(/[^a-zA-Z0-9]/, 'Possuir algum caractere especial'),
+  url: string(),
+});
 
 function Form() {
-  const [showForm, setShowForm] = useState(false);
+  const { register, handleSubmit: onSubmit,
+    formState: { errors, isValid, isDirty, isSubmitting } } = useForm(
+    { resolver: yupResolver(schema), mode: 'onChange' },
+  );
+  const [showForm, setShowForm] = useState<boolean>(false);
+
+  const handleSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  console.log(errors);
 
   return (
     <>
       { showForm
       && (
-        <form>
+        <form onSubmit={ onSubmit(handleSubmit) }>
           <label>
             Nome do serviço
-            <input type="text" />
+            <input
+              type="text"
+              { ...register('nome') }
+            />
           </label>
 
           <label>
             Login
-            <input type="text" />
+            <input type="text" { ...register('login') } />
           </label>
 
           <label>
             Senha
-            <input type="password" name="" id="" />
+            <input type="password" id="" { ...register('senha') } />
+            <h1 className="invalid-password-check">{errors?.senha?.message}</h1>
           </label>
 
           <label>
             URL
-            <input type="text" />
+            <input type="text" { ...register('url') } />
           </label>
-          <button>Cadastrar</button>
+          <button disabled={ !(isDirty && isValid) }>
+            Cadastrar
+          </button>
           <button onClick={ () => setShowForm(false) }>Cancelar</button>
         </form>)}
 
